c9f1191e697fa905a76a943c870b31db38dde337
Alexander Elias <alex.steven.elias@gmail.com>
2019-08-26 17:03:55 -0700

    optimized logo, removed old files, fixed menu, code clean up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-var menu = document.querySelector('.menu');
-var icon = document.querySelector('.icon');
 
-icon.addEventListener('click', function () {
-    menu.classList.toggle('active');
+document.querySelector('.menu-icon-open').addEventListener('click', function () {
+    document.querySelector('.menu').classList.add('active');
+});
+
+document.querySelector('.menu-icon-close').addEventListener('click', function () {
+    document.querySelector('.menu').classList.remove('active');
 });
